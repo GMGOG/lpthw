238e5a0bea28c38e6450d9386107993848887919
GMGOG <43651504+GMGOG@users.noreply.github.com>
2019-01-27 10:06:42 +0530

    Just started learning Python.

---
+++ Ex11.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+height = input("What is your height?")
+weight = input("How much do you weigh? ")
+
+print(f"so, you'are {name} , {height} tall and {weight} heavy.")
---
+++ Ex12.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:",script)
+print("your first variable",first)
+print("your second variable",second)
+print("your third variable",third)
